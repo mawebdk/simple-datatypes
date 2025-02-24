cf2733d4147c796c2827498adc3cdb09f1865544
Michael Risbjerg Amstrup <michael@maweb.dk>
2025-02-24 16:57:14 +0100

    Initial implementation of MAInteger and MAString with test.

---
+++ src/MAIntegerException.php
@@ -0,0 +1,9 @@
+<?php
+namespace MawebDK\SimpleDatatypes;
+
+use Exception;
+
+/**
+ * Represents an error raised by MAInteger or a subclass of MAInteger.
+ */
+class MAIntegerException extends Exception {}
